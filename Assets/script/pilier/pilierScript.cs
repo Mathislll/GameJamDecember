cd5939afaaf5abe72fc70ee3431a1820574c6043
maitre-claquer <131771924+maitre-claquer@users.noreply.github.com>
2023-12-11 10:58:44 +0100

    update pillard

---
+++ Assets/script/pilier/pilierScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pilierScript : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float pillardGap;
+
+    [SerializeField]
+    private GameObject topPillard;
+    [SerializeField]
+    private GameObject bottomPillard;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
