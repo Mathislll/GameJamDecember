f2053472207ca5242e09309fb45b48c27586e969
maitre-claquer <131771924+maitre-claquer@users.noreply.github.com>
2023-12-11 11:18:52 +0100

    pillard update script

--- Assets/script/pilier/pilierScript.cs
+++ Assets/script/pilier/pilierScript.cs
@@ -8,15 +8,32 @@ public class pilierScript : MonoBehaviour
     private float speed;
     [SerializeField]
     private float pillardGap;
+    [SerializeField]
+    private float pillardPos;
 
     [SerializeField]
     private GameObject topPillard;
     [SerializeField]
     private GameObject bottomPillard;
 
+    private float[] limitPillardPos = new float[] {4.25f,-4.25f};
+
     void Start()
     {
-        
+        if(pillardPos > limitPillardPos[0] || pillardPos < limitPillardPos[1])
+        {
+            Debug.LogError("la position du pilier va dépasser l'écran");
+            Debug.Break();
+            
+        }
+        if(pillardPos <= 0)
+        {
+            Debug.LogError("les pilier ne laissent pas d'ouverture");
+            Debug.Break();
+        }
+
+        topPillard.transform.localPosition += new Vector3(0,pillardGap,0);
+        transform.position = new Vector3(0,pillardPos,0);
     }
 
     void Update()
