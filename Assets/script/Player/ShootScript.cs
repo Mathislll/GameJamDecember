164f025aa5bfd0bdfdf7e5c6899ee07289d0e116
Mathis chv <mathischevry@gmail.com>
2023-12-11 13:08:40 +0100

    add projectile asset

--- Assets/script/Player/ShootScript.cs
+++ Assets/script/Player/ShootScript.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class ShootScript : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+
+    public float timer = 0.0f;
+    public float timeBetweenShots = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +23,15 @@ public class ShootScript : MonoBehaviour
         if (Input.GetButtonDown ("Fire") == true)
         {
             Shoot();
+        }
+        
+        timer += Time.deltaTime;
+        if (timer >= timeBetweenShots)
+        {
+            
+                Shoot();
+                timer = 0.0f;
+            
         }
     }
 
