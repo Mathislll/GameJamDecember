6732aa08b707db5327b3ca7ea2e081cdf0171dba
Mathis chv <mathischevry@gmail.com>
2023-12-11 14:26:04 +0100

    new level, update player

--- Assets/script/Player/ShootScript.cs
+++ Assets/script/Player/ShootScript.cs
@@ -7,36 +7,50 @@ public class ShootScript : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public GameObject bullet;
 
     public float timer = 0.0f;
     public float timeBetweenShots = 0.1f;
 
+    public float fireRate = 1.0f;  // Délai en secondes entre les tirs
+    private float nextFireTime = 0f;  // Temps prochain tir est autorisé
+
+    public bool autoShoot = false;
+    public bool isPlayerBullet = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown ("Fire") == true)
+        if (Input.GetButtonDown("Fire") && Time.time >= nextFireTime)
         {
+            nextFireTime = Time.time + fireRate;  // Mise à jour du temps pour le prochain tir
             Shoot();
         }
-        
-        timer += Time.deltaTime;
-        if (timer >= timeBetweenShots)
+
+        // autoshoot
+        if (autoShoot)
         {
-            
+            timer += Time.deltaTime;
+            if (timer >= timeBetweenShots)
+            {
+
                 Shoot();
                 timer = 0.0f;
-            
+
+            }
         }
     }
 
     public void Shoot()
     {
-        Instantiate(bulletPrefab, bulletSpawn.position, bulletPrefab.transform.rotation);
+        bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletPrefab.transform.rotation, transform.parent);
+        bullet.GetComponent<BulletScript>().BulletInit(transform, isPlayerBullet);
     }
 }
